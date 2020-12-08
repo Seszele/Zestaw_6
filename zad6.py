8396c671405a0fc7a28d3301c3618c6ff1d2fb87
Seszele <Seszele11@onet.pl>
2020-12-08 13:38:50 +0100

    kinda stolen

--- zad6.py
+++ zad6.py
@@ -4,15 +4,25 @@
 # Na przykład dla tablicy: [1,7,3,5,11,2] rozwiązaniem jest liczba 10.
 
 
-def lowest_R(arr, smh, s=0, i=-1, si=0, low=0):
+def lowest_R(arr, s=0, i=-1, si=0, low=0):
     if s == si != 0:
-        print(s)
-        return True
+        return (low, s)
     if i == len(arr)-1:
-        return False
-    return lowest_R(arr, smh, s+arr[i+1], i+1, si+i+1, 69) or lowest_R(arr, smh, s, i+1, si, 0)
+        return (len(arr)+1, -1)
+
+    a = lowest_R(arr, s+arr[i+1], i+1, si+i+1, low + 1)
+    b = lowest_R(arr, s, i+1, si, low)
+
+    return a if a[0] < b[0] else b
+
+
+def lowest(arr):
+    result = lowest_R(arr)
+    if result[1] != -1:
+        return result[1]
+    return None
 
 
 #    0  1  2  3  4   5         7 1 1    7 2 1    12 3 4
 T = [1, 7, 3, 5, 11, 2]
-print(lowest_R(T, []))
+print(lowest(T))
